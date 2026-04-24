0213fc4318a1eafc6bc45819203319f6dd3b7dac
Henderson590 <xsilwolfgamerx@gmail.com>
2026-04-23 22:24:34 -0500

    Still fixing

--- netlify/functions/promo.js
+++ netlify/functions/promo.js
@@ -2,7 +2,7 @@ export async function handler(event, context) {
   const promoCodes = [
     { code: "EARLY20", type: "percent", value: 20 },
     { code: "DEFIANCE10", type: "percent", value: 10 },
-    { code: "TESTING", type: "flat", value: 38.50 }
+    { code: "TESTING", type: "flat", value: 38 }
   ];
 
   const body = JSON.parse(event.body);
