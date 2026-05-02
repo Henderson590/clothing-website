fbc42be93d5e2875db7f142747e41daf1ecf4f23
Henderson590 <xsilwolfgamerx@gmail.com>
2026-05-02 16:19:59 -0500

    testing over

--- src/data/products.js
+++ src/data/products.js
@@ -77,7 +77,7 @@ export const products = [
     ], // INSERT PRODUCT IMAGE HERE (recommended size: 1200x1200)
     featured: false,
     limitedEdition: false,
-    disabled: false,
+    disabled: true,
     newArrival: true
   },
    {
